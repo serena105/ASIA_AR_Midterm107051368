15ead4739af77c2802a9cddfc6da3203358b8591
serena105 <71679914+serena105@users.noreply.github.com>
2020-11-17 16:06:48 +0800

    完成版

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{   [Header("男孩")]
-    public Transform boy;
+{   [Header("女人")]
+    public Transform women;
     [Header("女孩")]
     public Transform girl;
     [Header("虛擬搖桿")]
     public FixedJoystick joystick;
-    [Header("Boy旋轉速度"), Range(1f, 60f)]
+    [Header("Women旋轉速度"), Range(1f, 60f)]
     public float turnD = 1.2f;
     [Header("Girl旋轉速度"), Range(1f, 60f)]
     public float turnC = 1.2f;
     [Header("縮放"), Range(0.01f, 1f)]
     public float size = 0.06f;
-    [Header("男孩動畫元件")]
-    public Animator aniBoy;
+    [Header("女人動畫元件")]
+    public Animator aniWomen;
     [Header("女孩動畫元件")]
     public Animator aniGirl;
 
@@ -28,10 +28,10 @@ public class GameManager : MonoBehaviour
         print("更新事件");
         print(joystick.Vertical);
 
-        boy.Rotate(0, joystick.Horizontal * turnD, 0);
+        women.Rotate(0, joystick.Horizontal * turnD, 0);
         girl.Rotate(0, joystick.Horizontal * turnC, 0);
-        boy.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
-        boy.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(boy.localScale.x, 0.3f, 3f);
+        women.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
+        women.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(women.localScale.x, 0.3f, 3f);
         girl.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
         girl.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(girl.localScale.x, 0.3f, 3f);
     }
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
     public void PlayAnimation(string aniName)
     {
         print(aniName);
-        aniBoy.SetTrigger(aniName);
+        aniWomen.SetTrigger(aniName);
         aniGirl.SetTrigger(aniName);
     }
 
